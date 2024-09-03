ceb891c41f41412bb0f4c36b270c11b1b0867c41
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2024-09-03 18:51:45 +0530

    add bg img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import headerImg from '../../assets/header_img.png'; // Import the image
 
 const Header = () => {
   return (
-    <div className='Header'>
-<div className="header-contents">
-  <h2>Order your favourite food here</h2>
-  <p>Choose from a diverse menu featuring a detacting array of dishes crafted with the finest ingredient and satisfy your craving and elevate your dining experience , On the delicious meal at a time.</p>
-  <button>View Menu</button>
-</div>
-</div>
-  )
+    <div className='Header' style={{ backgroundImage: `url(${headerImg})` }}>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a detacting array of dishes crafted with the finest ingredient and satisfy your craving and elevate your dining experience, one delicious meal at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
